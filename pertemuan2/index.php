f58000818961c203340d39556e5de1e335c9fc66
203040019Ikhsan <ikhsanardiansyah1800@gmail.com>
2021-02-15 00:49:07 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,63 @@
+<?php
+/*
+Ikhsan Ardiansyah
+203040019
+https://github.com/203040019Ikhsan/pw2021_203040019.git
+Pertemuan 2 
+Mempelajari mengenai sintaks PHP
+*/
+?>
+<?php 
+// Pertemuan 2 - PHP Dasar
+// Stintaks PHP
+
+// Standar Output
+// echo, Print
+// print_r
+// var_dumb
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe data
+// Variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Ikhsan Ardiansyah";
+
+// echo "Halo, nama saya $nama"
+
+// Operator
+// arimatika
+// + - * / %
+// echo $x * $y;
+
+// penggabung string / concatenation / concat
+// .
+// $nama_depan = "Ikhsan";
+// $nama_belakang = "Ardiansyah";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assigment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Ikhsan";
+// $nama .= " ";
+// $nama .= "Ardiansyah";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// var_dump(1 == "1");
+
+// identitas
+// ===, !==
+// var_dump(1 === "1")
+
+// Logika
+// &&, ||, !
+// $x = 10;
+// var_dump($x < 20 || $x % 2 == 0);
+?>
